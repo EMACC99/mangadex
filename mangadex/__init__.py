cc84a85b61a9ec1cc21a9cb916fcfc9ad4f00ce0
emacc99 <lalitoceja@gmail.com>
2021-05-16 17:06:42 -0500

    changed version number

--- mangadex/__init__.py
+++ mangadex/__init__.py
@@ -5,6 +5,6 @@ from .models import (Manga, Tag, Chapter, User, ScanlationGroup, Author)
 from .api import (Api)
 
 __author__ = 'Eduardo Ceja'
-__version__ = "0.0.21"
+__version__ = "0.0.3"
 __license__ = "MIT"
 __copytight__ = "Copyright (c) 2021 Eduardo Ceja"
