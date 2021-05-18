5e884945535dc710fb6bd941e1b27345143bb187
emacc99 <lalitoceja@gmail.com>
2021-05-18 00:20:01 -0500

    fixed setup py repeated keyword

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.3.0'
+VERSION = '1.3.1'
 DESCRIPTION = 'Python wrapper for the mangadex API'
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
