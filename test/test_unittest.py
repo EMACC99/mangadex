7b07210d7e64a4e086c6f86f288f9849a290d095
emacc99 <lalitoceja@gmail.com>
2021-09-24 19:16:49 -0500

    Testing no longer relying in saved JSON responses

--- test/test_unittest.py
+++ test/test_unittest.py
@@ -18,99 +18,79 @@ class TestApi():
         
     def test_SearchManga(self):
         # we're going to search for the iris zero
-        failed = False
         resp = self.api.get_manga_list(title = "iris zero", limit = 1)[0]
 
-        try:
-            saved_resp = read_json_files("test/saved_search_manga_response.json")
-        except FileNotFoundError:
-            failed = True
-        saved_resp = mangadex.Manga._create_manga(saved_resp["results"][0])
+        url = f"{self.api.URL}/manga"
+        raw_response = mangadex.URLRequest._request_url(url, "GET", timeout = self.timeout, params={"limit" : 1, "title" : "iris zero"})
 
-        if failed:
-            pytest.fail("File not found")
+        saved_resp = mangadex.Manga._create_manga(raw_response["data"][0])
 
         assert resp == saved_resp, "The Manga objects are not equal"
     
     def test_GetMangaChapter(self):
-        failed = False
-        resp = self.api.get_chapter(id = "015979c8-ffa4-4afa-b48e-3da6d10279b0")
-        try:
-            saved_resp = read_json_files("test/saved_get_chapter_response.json")
-        except FileNotFoundError:
-            failed = True
-            
-        saved_resp = mangadex.Chapter._create_chapter(saved_resp)
+        ch_id = "015979c8-ffa4-4afa-b48e-3da6d10279b0"
+        resp = self.api.get_chapter(id = ch_id)
 
-        if failed:
-            pytest.fail("File not found, test could not be completed")
-        
+        url = f"{self.api.URL}/chapter/{ch_id}"
+        raw_response = mangadex.URLRequest._request_url(url, "GET", timeout = self.timeout, params= {"id" : ch_id})
+
+        saved_resp = mangadex.Chapter._create_chapter(raw_response)        
+
         assert resp == saved_resp,  "The Chapter Objects are not equal"
 
     def test_GetAuthor(self):
-        failed = False
-        resp = self.api.get_author_by_id(id = "df765fdc-ea9f-45d0-9191-d95615662d49")
-        try:
-            saved_resp = read_json_files("test/saved_get_author_id_response.json")
-        except:
-            failed = True
-        
-        saved_resp = mangadex.Author._create_author(saved_resp)
 
-        if failed:
-            pytest.fail("File not found, test could not be completed")
-        
+        author_id = "df765fdc-ea9f-45d0-9191-d95615662d49"
+
+        resp = self.api.get_author_by_id(id = author_id)
+
+        url = f"{self.api.URL}/author/{author_id}"
+
+        raw_respone  = mangadex.URLRequest._request_url(url, "GET", timeout = self.timeout, params = {"id" : author_id})
+
+        saved_resp = mangadex.Author._create_author(raw_respone)
+
         assert resp == saved_resp, "The Author Objects are not equal"
     
     def test_GetTags(self):
-        failed = False
         resp = self.api.tag_list()
-        try:
-            saved_reps = read_json_files("test/saved_get_tag_response.json")
-        except FileNotFoundError:
-            failed = True
-        
-        if failed: 
-            pytest.fail("File not found, test could not be completed")
-        
-        saved_reps = mangadex.Tag._create_tag_list(saved_reps)
+
+        url = f"{self.api.URL}/manga/tag"
+        raw_response = mangadex.URLRequest._request_url(url, "GET", timeout = self.timeout)
+        saved_reps = mangadex.Tag._create_tag_list(raw_response)
 
         assert resp == saved_reps, "The test objects are not equal"
 
     def test_GetMangaChaptersAndVolumes(self):
         #lets use iris zero as is in hiatus
-        failed = False
-        resp = self.api.get_manga_volumes_and_chapters(id = "786ff721-8fd3-413d-8e50-938d8b06f917")
+        manga_id = "786ff721-8fd3-413d-8e50-938d8b06f917"
+        resp = self.api.get_manga_volumes_and_chapters(id = manga_id)
 
-        try:
-            saved_resp = read_json_files("test/saved_get_manga_chaps_and_volumes_response.json")
-        except FileNotFoundError:
-            failed = True
+        url = f"{self.api.URL}/manga/{manga_id}/aggregate"
         
-        if failed:
-            pytest.fail("File not found, test could not be completed")
+        raw_response = mangadex.URLRequest._request_url(url, "GET", timeout = self.timeout)
+    
+        saved_resp = raw_response["volumes"]
 
-        saved_resp = saved_resp["volumes"]
         assert resp == saved_resp, "The values are not equal"
 
-    # def test_save_jsons(self):
-    #     url = f"{self.api.URL}/group"
-    #     params = {"ids[]" : "f5f83084-ec42-4354-96fd-1b637a89b8b3"}
-    #     with open("test/saved_get_scan_group_response.json", "w+") as f:
-    #         json.dump(mangadex.URLRequest._request_url(url, "GET", timeout=self.timeout, params=params),f)
 
     def test_GetScanlationGroup(self):
-        failed = False
-        resp = self.api.scanlation_group_list(group_ids = "f5f83084-ec42-4354-96fd-1b637a89b8b3") # black cat scanlations
+        ids = ["f5f83084-ec42-4354-96fd-1b637a89b8b3"]
+        resp = self.api.scanlation_group_list(group_ids = ids) # black cat scanlations
 
-        try:
-            saved_response = read_json_files("test/saved_get_scan_group_response.json")
-        except FileNotFoundError:
-            failed = True
-        
-        if failed:
-            pytest.fail("File not found, test could not be completed")
+        url = f"{self.api.URL}/group"
+        raw_response = mangadex.URLRequest._request_url(url, "GET", timeout = self.timeout, params = {"ids[]" : ids})
 
-        saved_response = mangadex.ScanlationGroup._create_group_list(saved_response)
+        saved_response = mangadex.ScanlationGroup._create_group_list(raw_response)
+
         assert resp == saved_response
         
+    # def test_GetChapter(self):
+    #     raise NotImplementedError
+
+    # def test_FetchCoverImages(self):
+    #     raise NotImplementedError
+
+    # def test_(self):
+    #     raise NotImplementedError
