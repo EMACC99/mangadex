5d63c999cf9965841b9bbfc54739ed04d6d327e6
EMACC <lalitoceja@gmail.com>
2022-01-22 11:56:17 -0600

    version control

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '2.5.1'
+VERSION = '2.5.2'
 DESCRIPTION = 'Python wrapper for the mangadex API'
 with open("README.md", "r", encoding="utf8") as f:
     LONG_DESCRIPTION = f.read()
